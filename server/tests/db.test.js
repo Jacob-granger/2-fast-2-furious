0f75d37450be7d3d36325d9d27945a205d9f1736
Lari Ruecker <122192082+popoxta@users.noreply.github.com>
2023-08-11 14:16:55 +1200

    Remove boilerplate tests

--- server/tests/db.test.js
+++ server/tests/db.test.js
@@ -17,18 +17,3 @@ afterAll(async () => {
   await db.connection.destroy()
 })
 
-test('getUsers gets all users', async () => {
-  // One for each letter of the alphabet!
-  const expected = 3
-  const users = await db.getUsers(testDb)
-  const actual = users.length
-  expect(actual).toBe(expected)
-})
-
-test('getUser gets a single user', async () => {
-  const expected = 'test user 1'
-  const user = await db.getUser(99901, testDb)
-  const actual = user.name
-
-  expect(actual).toBe(expected)
-})
